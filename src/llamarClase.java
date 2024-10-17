542ee65e12cdf19d81e8b3131d3cf1b63a84253a
Diego Mullo <diego.mullo@epn.edu.ec>
2024-10-17 09:48:12 -0500

    commit 2 y final

--- src/llamarClase.java
+++ src/llamarClase.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class llamarClase {
+    public static void main(String[] args) {
+        vehiculos auto1 = new vehiculos("Toyota",2022,"Verde");
+        vehiculos auto2 = new vehiculos("Camaro",2023,"Amarillo");
+        auto1.imprimir();
+        auto2.imprimir();
+
+        vehiculos autoModelo = new vehiculos("Chiron");
+        System.out.println();
+        autoModelo.imprimirModelo();
+
+        vehiculos auto3 = new vehiculos("Bugatti","Chiron",2023,"Negro","Nuevo");
+        System.out.println();
+        auto3.imprimirDos();
+
+        vehiculos auto4 = new vehiculos();
+        System.out.println();
+        auto4.imprimir();
+    }
 }
--- src/vehiculos.java
+++ src/vehiculos.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class vehiculos {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    String marca;
+    String modelo;
+    int anio;
+    String color;
+    String condicion;
+
+    public vehiculos(String marca, String modelo, int anio, String color, String condicion){
+        this.marca=marca;
+        this.modelo=modelo;
+        this.anio=anio;
+        this.color=color;
+        this.condicion=condicion;
     }
+
+    public void imprimirDos(){
+        System.out.println("La marca es: "+marca+". El modelo es: "+modelo+". El año es: "+anio+". El color es: "+color+". La condicion es: "+ condicion);
+    }
+
+    public vehiculos(String modelo, int anio, String color){// metodo constructor para inicializar las variables
+        this.modelo = modelo;
+        this.anio = anio;
+        this.color = color;
+    }
+
+    public void imprimir(){//metodo imprimir
+        System.out.println("El módelo del vehiculo es: "+modelo+". El año es: "+anio+" y el color es: "+color);
+    }
+
+    public vehiculos(String modelo){
+        this.modelo = modelo;
+    }
+
+    public void imprimirModelo(){
+        System.out.println("El módelo del carro es: "+modelo);
+    }
+
+    public vehiculos(){
+        this.modelo = "R8";
+        this.anio = 2008;
+        this.color = "Rojo";
+    }
+
 }
